e7e1e021cd62d04d7637ecf9e34cbde25e7944c1
Tesfay-Teshome <tesfix2017@gmail.com>
2023-06-24 15:53:54 +0300

    working with views

--- alxse/base/views.py
+++ alxse/base/views.py
@@ -2,8 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse, request
 
 # Create your views here.
+rooms = [
+    {'id': 1, 'name': 'Lets Learn Python'},
+    {'id': 2, 'name': 'Lets Learn Django'},
+    {'id': 3, 'name': 'Lets Learn Javascript'},
+    {'id': 4, 'name': 'Lets Learn React'},
+    {'id': 5, 'name': 'Lets Learn Vue'},
+    {'id': 6, 'name': 'Lets Learn Angular'},
+    {'id': 7, 'name': 'Lets Learn Node'},
+]
 def home(request):
-    return render(request, "home.html")
+    context = {'rooms': rooms }
+    return render(request, "home.html", context)
 
 def room(request):
     return render(request, "room.html")
